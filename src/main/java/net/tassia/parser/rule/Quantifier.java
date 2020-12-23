2da99d65c3b84a357cc03057d1443d529c5e7ae1
Tassilo <tassia710@gmail.com>
2020-12-23 15:24:37 +0100

    Add JavaDoc

--- src/main/java/net/tassia/parser/rule/Quantifier.java
+++ src/main/java/net/tassia/parser/rule/Quantifier.java
@@ -1,13 +1,30 @@
 package net.tassia.parser.rule;
 
+/**
+ * Defines how many times a given pattern should be matched.
+ * @since Parser 1.0
+ * @author Tassilo
+ */
 public enum Quantifier {
 
+	/**
+	 * The given pattern must be matched once.
+	 */
 	ONCE,
 
+	/**
+	 * The given pattern can be matched once.
+	 */
 	OPTIONAL,
 
+	/**
+	 * The given pattern must be matched at least once.
+	 */
 	MULTIPLE,
 
+	/**
+	 * The given pattern can be matched an undefined amount of times.
+	 */
 	ANY
 
 }

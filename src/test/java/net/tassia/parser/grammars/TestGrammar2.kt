d914238513e1d12435d95bb8aeab651aa8e94143
Tassilo <tassia710@gmail.com>
2021-03-29 20:27:14 +0200

    wtf

--- src/test/java/net/tassia/parser/grammars/TestGrammar2.kt
+++ src/test/java/net/tassia/parser/grammars/TestGrammar2.kt
@@ -8,7 +8,7 @@ import net.tassia.parser.token.TokenType
 object TestGrammar2 {
 
 	// <DIGIT> ::= "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-	val DIGIT	by !where(Char::isDigit)
+	val DIGIT	by where(Char::isDigit)
 
 
 
